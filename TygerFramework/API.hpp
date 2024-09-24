215d36016c94a8e85eda1ed4cf3083c62d1ec79b
ElusiveFluffy <Freya8822@hotmail.com>
2024-09-24 18:15:14 +0930

    Remove namespace from API

--- TygerFramework/API.hpp
+++ TygerFramework/API.hpp
@@ -2,103 +2,101 @@
 #include <memory>
 #include <stdexcept>
 
-namespace TygerFrameworkAPI {
-	class API {
-	public:
-		//Will default to the plugin's file name on initialization, the name will be used in the logs
-		//If you want a different name, can change the plugin name in DllMain
-		static inline std::string PluginName = "Plugin";
+class API {
+public:
+	//Will default to the plugin's file name on initialization, the name will be used in the logs
+	//If you want a different name, can change the plugin name in DllMain
+	static inline std::string PluginName = "Plugin";
 
-		static auto& Initialize(const TygerFrameworkPluginInitializeParam* param) {
-			if (param == nullptr) {
-				throw std::runtime_error("param is null");
-			}
-
-			if (mInstance != nullptr) {
-				throw std::runtime_error("API already initialized");
-			}
-
-			mInstance = std::make_unique<API>(param);
-
-			//If the plugin name has already been changed to something custom it won't change it to the file name
-			if (PluginName == "Plugin")
-				PluginName = param->pluginFileName;
-
-			return mInstance;
+	static auto& Initialize(const TygerFrameworkPluginInitializeParam* param) {
+		if (param == nullptr) {
+			throw std::runtime_error("param is null");
 		}
 
-		//Only call this AFTER calling initialize
-		static auto& Get() {
-			if (mInstance == nullptr) {
-				throw std::runtime_error("API not initialized");
-			}
-
-			return mInstance;
+		if (mInstance != nullptr) {
+			throw std::runtime_error("API already initialized");
 		}
 
-		API(const TygerFrameworkPluginInitializeParam* param)
-			: mParam{ param }
-		{
+		mInstance = std::make_unique<API>(param);
+
+		//If the plugin name has already been changed to something custom it won't change it to the file name
+		if (PluginName == "Plugin")
+			PluginName = param->pluginFileName;
+
+		return mInstance;
+	}
+
+	//Only call this AFTER calling initialize
+	static auto& Get() {
+		if (mInstance == nullptr) {
+			throw std::runtime_error("API not initialized");
 		}
 
-		inline const auto param() const {
-			return mParam;
-		}
+		return mInstance;
+	}
 
-		//Gets the current Ty window handle
-		static HWND GetTyWindowHandle() {
-			return API::Get()->param()->functions->GetTyWindowHandle();
-		}
+	API(const TygerFrameworkPluginInitializeParam* param)
+		: mParam{ param }
+	{
+	}
 
-		/// <summary>
-		/// Gets the current Ty Game
-		/// </summary>
-		/// <returns>0: Couldn't detect which game
-		/// <para>1: Ty 1</para>
-		/// <para>2: Ty 2</para>
-		/// <para>3: Ty 3</para></returns>
-		static int WhichTyGame() {
-			return Get()->param()->functions->WhichTyGame();
-		}
+	inline const auto param() const {
+		return mParam;
+	}
 
-		//Writes a message to the console and the log file. Default log level is info
-		static void LogPluginMessage(std::string message, LogLevel logLevel = Info) {
-			Get()->param()->functions->LogPluginMessage("[" + PluginName + "] " + message, logLevel);
-		}
+	//Gets the current Ty window handle
+	static HWND GetTyWindowHandle() {
+		return API::Get()->param()->functions->GetTyWindowHandle();
+	}
 
-		//Sets the imgui font to be the same as TygerFramework's main window
-		static void SetImGuiFont(void* imguiFont) {
-			Get()->param()->functions->SetImGuiFont(imguiFont);
-		}
+	/// <summary>
+	/// Gets the current Ty Game
+	/// </summary>
+	/// <returns>0: Couldn't detect which game
+	/// <para>1: Ty 1</para>
+	/// <para>2: Ty 2</para>
+	/// <para>3: Ty 3</para></returns>
+	static int WhichTyGame() {
+		return Get()->param()->functions->WhichTyGame();
+	}
 
-		//Sets all the elements for drawing elements from the plugin in the TygerFramework window
-		//Overwrites the old value if its called again
-		static void SetTygerFrameworkImGuiElements(std::vector<TygerFrameworkImGuiParam> elements) {
-			Get()->param()->functions->SetTyFImGuiElements(PluginName, elements);
-		}
+	//Writes a message to the console and the log file. Default log level is info
+	static void LogPluginMessage(std::string message, LogLevel logLevel = Info) {
+		Get()->param()->functions->LogPluginMessage("[" + PluginName + "] " + message, logLevel);
+	}
 
-		//--------------------------
-		//Event subscriber functions
-		//--------------------------
+	//Sets the imgui font to be the same as TygerFramework's main window
+	static void SetImGuiFont(void* imguiFont) {
+		Get()->param()->functions->SetImGuiFont(imguiFont);
+	}
 
-		static bool AddDrawPluginUI(DrawUIFunc func) {
-			return Get()->param()->functions->AddDrawPluginUI(PluginName, func);
-		}
+	//Sets all the elements for drawing elements from the plugin in the TygerFramework window
+	//Overwrites the old value if its called again
+	static void SetTygerFrameworkImGuiElements(std::vector<TygerFrameworkImGuiParam> elements) {
+		Get()->param()->functions->SetTyFImGuiElements(PluginName, elements);
+	}
 
-		static bool AddPluginImGuiHasFocus(ImGuiHasFocusFunc func) {
-			return Get()->param()->functions->AddPluginImGuiHasFocus(PluginName, func);
-		}
+	//--------------------------
+	//Event subscriber functions
+	//--------------------------
 
-		static bool AddPluginWndProc(WndProcFunc func) {
-			return Get()->param()->functions->AddPluginWndProc(PluginName, func);
-		}
+	static bool AddDrawPluginUI(DrawUIFunc func) {
+		return Get()->param()->functions->AddDrawPluginUI(PluginName, func);
+	}
 
-		static bool AddTickBeforeGame(TickBeforeGameFunc func) {
-			return Get()->param()->functions->AddTickBeforeGame(PluginName, func);
-		}
+	static bool AddPluginImGuiHasFocus(ImGuiHasFocusFunc func) {
+		return Get()->param()->functions->AddPluginImGuiHasFocus(PluginName, func);
+	}
 
-	private:
-		static inline std::unique_ptr<API> mInstance;
-		const TygerFrameworkPluginInitializeParam* mParam;
-	};
-}
+	static bool AddPluginWndProc(WndProcFunc func) {
+		return Get()->param()->functions->AddPluginWndProc(PluginName, func);
+	}
+
+	static bool AddTickBeforeGame(TickBeforeGameFunc func) {
+		return Get()->param()->functions->AddTickBeforeGame(PluginName, func);
+	}
+
+private:
+	static inline std::unique_ptr<API> mInstance;
+	const TygerFrameworkPluginInitializeParam* mParam;
+};
