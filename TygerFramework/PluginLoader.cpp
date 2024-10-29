61e028d4a00d0ee45a1f661059cbd33bcef4dcbd
ElusiveFluffy <Freya8822@hotmail.com>
2024-10-29 13:51:00 +1030

    Make the early plugin init provide a error message when a plugin fails to load

--- TygerFramework/PluginLoader.cpp
+++ TygerFramework/PluginLoader.cpp
@@ -44,6 +44,32 @@ TygerFrameworkPluginInitializeParam pluginInitParam{
     &pluginFunctions
 };
 
+//Returns the last Win32 error, in string format. Returns an empty string if there is no error.
+std::string GetLastErrorAsString(DWORD errorCode)
+{
+    if (errorCode == 0) {
+        return std::string(); //No error message has been recorded
+    }
+
+    LPSTR messageBuffer = nullptr;
+
+    //Ask Win32 to give us the string version of that message ID.
+    //The parameters we pass in, tell Win32 to create the buffer that holds the message for us (because we don't yet know how long the message string will be).
+    size_t size = FormatMessageA(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
+        NULL, errorCode, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (LPSTR)&messageBuffer, 0, NULL);
+
+    //Copy the error message into a std::string.
+    std::string message(messageBuffer, size);
+    //Remove the new line
+    message.pop_back();
+    message.pop_back();
+
+    //Free the Win32's string's buffer.
+    LocalFree(messageBuffer);
+
+    return message;
+}
+
 void PluginLoader::DependencyInit() try {
 
     fs::path dependencyPath = TygerFramework::GetDependencyDir();
@@ -63,8 +89,10 @@ void PluginLoader::DependencyInit() try {
             FrameworkInstance->LogMessage("[Plugin Loader] Loading: " + path.stem().string());
 
             if (module == nullptr) {
-                FrameworkInstance->LogMessage("[Plugin Loader] Failed to Load Dependency: " + path.stem().string(), TygerFramework::Error);
-                mDependencyErrors.emplace(path.stem().string(), "Failed to Load");
+                DWORD errorCode = GetLastError();
+                std::string errorMessage = GetLastErrorAsString(errorCode);
+                FrameworkInstance->LogMessage("[Plugin Loader] Failed to Load Dependency: " + path.stem().string() + ", With the Error: " + errorMessage, TygerFramework::Error);
+                mDependencyErrors.emplace(path.stem().string(), "Failed to Load With the Error: " + errorMessage);
                 continue;
             }
 
@@ -100,8 +128,10 @@ void PluginLoader::PluginEarlyInit() try {
             FrameworkInstance->LogMessage("[Plugin Loader] Loading: " + path.stem().string());
 
             if (module == nullptr) {
-                FrameworkInstance->LogMessage("[Plugin Loader] Failed to Load Plugin: " + path.stem().string(), TygerFramework::Error);
-                mPluginErrors.emplace(path.stem().string(), "Failed to Load");
+                DWORD errorCode = GetLastError();
+                std::string errorMessage = GetLastErrorAsString(errorCode);
+                FrameworkInstance->LogMessage("[Plugin Loader] Failed to Load Plugin: " + path.stem().string() + ", With the Error: " + errorMessage + "(" + std::to_string(errorCode) + ")", TygerFramework::Error);
+                mPluginErrors.emplace(path.stem().string(), "Failed to Load: " + errorMessage + "(" + std::to_string(errorCode) + ")");
                 continue;
             }
 
