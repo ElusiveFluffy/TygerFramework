0c3f9b1c54a255761b3e76f09337d66dfa356f24
Kana <102465188+ElusiveFluffy@users.noreply.github.com>
2026-07-07 11:19:00 +0930

    Update version number

--- TygerFramework/TygerFrameworkAPI.hpp
+++ TygerFramework/TygerFrameworkAPI.hpp
@@ -13,7 +13,7 @@
 
 constexpr int TygerFrameworkPluginVersion_Major = 1;
 constexpr int TygerFrameworkPluginVersion_Minor = 2;
-constexpr int TygerFrameworkPluginVersion_Patch = 0;
+constexpr int TygerFrameworkPluginVersion_Patch = 1;
 
 struct TygerFrameworkPluginVersion {
 	int Major;
